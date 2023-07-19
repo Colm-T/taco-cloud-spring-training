f56ca7091f2678ade033678f8d118f9b48a7e854
Colm T <colm.kl.tang@gmail.com>
2023-07-19 13:44:29 +0100

    initial spring project setup

---
+++ src/main/java/com/example/tacocloudspringtraining/TacoCloudSpringTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudspringtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudSpringTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudSpringTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tacocloudspringtraining/TacoCloudSpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudspringtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudSpringTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
